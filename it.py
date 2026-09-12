6fa055d3c4a4d6eccbbcb3cceed85feab41e89cb
Dapo <adewole776@gmail.com>
2026-09-12 12:27:30 -0700

    second commit

--- it.py
+++ it.py
@@ -16,7 +16,7 @@ print(df.head(3))
 # print(df['Priority'].unique())
 # print(df['Country'].unique())
 
-#
+# encoding   data
 ohe = OneHotEncoder(handle_unknown='ignore',
                     sparse_output=False,).set_output(transform='pandas')
 ohe_transformer = ohe.fit_transform(df[['Product group',
@@ -25,20 +25,23 @@ ohe_transformer = ohe.fit_transform(df[['Product group',
                                         'Agent Group',
                                         'Country'
                                         ]])
+# mapping my ordinal  categories  to numerical
 df['Support Level'] = df['Support Level'].map({'L1': 1, 'L2': 2, 'L3': 3})
 df['Priority'] = df['Priority'].map(
     {'Low': 0,  'Medium': 1, 'High': 2, 'Critical': 3})
+# concatenating  the new column  and dropping the previous one
 df = pd.concat([df.reset_index(drop=True), ohe_transformer.reset_index(drop=True)], axis=1).drop(columns=['Product group',
                                                                                                           'Source',
                                                                                                           'Topic',
                                                                                                           'Agent Group',
                                                                                                           'Country'])
-
+# converting the created time and resolution times into proper datatimeformat
 df['Created time'] = pd.to_datetime(df['Created time'])
 df['Resolution time'] = pd.to_datetime(df['Resolution time'])
+# Crearing resolution hours from created and resolution time which will serve as the target variable
 df['Resolution Hours'] = (
     df['Resolution time']-df['Created time']).dt.total_seconds()/3600
-
+# Dropping columns that could cause leakage  or affect the model
 df = df.drop(columns=['Status',
                       'Ticket ID',
                       'Agent Name',
@@ -52,20 +55,21 @@ df = df.drop(columns=['Status',
                       'Close time',
                       'Agent interactions', 'Survey results',
                       'Latitude', 'Longitude'])
-
+# splitting  the dataset into features (X) and target (y) variable
 X = df.drop(columns='Resolution Hours')
 y = df['Resolution Hours']
-
+# Training and testing the model
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=34)
 
 rd = RandomForestRegressor(random_state=12)
 rd.fit(X_train, y_train)
+# predicting the model
 y_pred = rd.predict(X_test)
 mea = mean_absolute_error(y_pred, y_test)
 mse = mean_squared_error(y_pred, y_test)
 r2_s = r2_score(y_pred, y_test)
-
+# tunning my dataset for a better result
 param_grid = {
     'n_estimators': [100, 200],
     'max_depth': [10, 20],
@@ -89,11 +93,11 @@ print(r2_s)
 print(rd_cv.best_params_)
 
 y_test_reset = y_test.reset_index(drop=True)
-
+# creating a Dataframe to compare predictions with actual values
 results = pd.DataFrame({
     "Actual Resolution Hours": y_test_reset,
     "predicted Resolution Hours": y_pred2
 })
-
+# making my results a csv file
 results.to_csv("resolution_prediction.csv", index=False)
 print(results.head())
